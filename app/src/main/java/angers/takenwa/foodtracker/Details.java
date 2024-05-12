ca4f8377054ea43c0d795522b8490660810b798d
Folley-05 <aristotepascaldjounda@gmail.com>
2024-05-12 16:37:04 +0200

    add logic for list activity and detail activity

--- app/src/main/java/angers/takenwa/foodtracker/Details.java
+++ app/src/main/java/angers/takenwa/foodtracker/Details.java
@@ -1,5 +1,6 @@
 package angers.takenwa.foodtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import angers.takenwa.foodtracker.databinding.ActivityDetailsBinding;
+import angers.takenwa.foodtracker.databinding.ActivityListBinding;
+
 public class Details extends AppCompatActivity {
 
+    ActivityDetailsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_details);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        binding=ActivityDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Intent intent=this.getIntent();
+
+        if (intent!=null){
+            String name=intent.getStringExtra("name");
+            String date=intent.getStringExtra("date");
+            int image=intent.getIntExtra("image", R.drawable.beer);
+
+            binding.detailName.setText(name);
+            binding.detailDate.setText(date);
+            binding.detailImage.setImageResource(image);
+        }
     }
 }
--- app/src/main/java/angers/takenwa/foodtracker/List.java
+++ app/src/main/java/angers/takenwa/foodtracker/List.java
@@ -1,6 +1,10 @@
 package angers.takenwa.foodtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,49 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
+import angers.takenwa.foodtracker.databinding.ActivityListBinding;
+import angers.takenwa.foodtracker.databinding.ActivityMainBinding;
+
 public class List extends AppCompatActivity {
+
+    ActivityListBinding binding;
+    ListAdapter listAdapter;
+    ArrayList<ListData> dataArrayList=new ArrayList<>();
+    ListData listData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_list);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_list);
+        binding= ActivityListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int[] imageList={R.drawable.pizza, R.drawable.egg, R.drawable.burger, R.drawable.fries, R.drawable.soda, R.drawable.beer};
+        String[] nameList={"Pizza", "Eggs", "Burger", "Fries", "Soda", "Beer"};
+        String[] dateList={"01.01.2025","01.01.2025","01.01.2025","01.01.2025","01.01.2025"};
+
+        for (int i = 0; i < imageList.length; i++) {
+            listData=new ListData(nameList[i], dateList[i], imageList[i]);
+            dataArrayList.add(listData);
+        }
+
+        listAdapter=new ListAdapter(List.this, dataArrayList);
+        binding.listview.setAdapter(listAdapter);
+        binding.listview.setClickable(true);
+
+        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                Intent intent=new Intent(List.this, Details.class);
+                intent.putExtra("name", nameList[i]);
+                intent.putExtra("date", dateList[i]);
+                intent.putExtra("image", imageList[i]);
+                startActivity(intent);
+            }
         });
+
     }
 }
